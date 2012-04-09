26b5614af9fbdb3247861a6a94e96d2657be2eee
Owain R Hunt <owainhunt@btinternet.com>
2012-04-09 21:53:18 +0100

    Initial Commit

---
+++ TableView/UAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UAAppDelegate.h
+//  TableView
+//
+//  Created by Owain Hunt on 09/04/2012.
+//  Copyright (c) 2012 Owain R Hunt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface UAAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TableView/UAAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  UAAppDelegate.m
+//  TableView
+//
+//  Created by Owain Hunt on 09/04/2012.
+//  Copyright (c) 2012 Owain R Hunt. All rights reserved.
+//
+
+#import "UAAppDelegate.h"
+
+@implementation UAAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
