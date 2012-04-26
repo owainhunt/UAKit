c52a91a22d09c0a3b02a17b58e21f40a909ad2bd
Owain R Hunt <owainhunt@btinternet.com>
2012-04-26 21:22:43 +0100

    UAViews now setTranslatesAutoresizingMaskIntoConstraints:NO.

--- UAKit/UAView.m
+++ UAKit/UAView.m
@@ -15,6 +15,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code here.
+        [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     }
     
     return self;
