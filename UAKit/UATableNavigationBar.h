a250caedd0caee36136406be0fc459eb4fd19d07
Owain R Hunt <owainhunt@btinternet.com>
2012-05-10 22:13:44 +0100

    Add UATableNavigationBar.
    Gradienty background FTW.

---
+++ UAKit/UATableNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  UATableNavigationBar.h
+//  UAKit
+//
+//  Created by Owain Hunt on 10/05/2012.
+//  Copyright (c) 2012 Owain R Hunt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface UATableNavigationBar : NSView
+
+@end
